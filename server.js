49e346bc3848381b9d58c315521c8faa51feb070
ronya78@gmail.com <ronya78@gmail.com>
2022-01-17 16:35:22 +0200

    Add code

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function (request, response){
+app.get("/", function (req, res){
   //console.log(request);
-  response.send("<h1>Hello, world!</h1>");
+  res.send("<h1>Hello, world!</h1>");
 });
+
+app.get("/contact", function(req,res){
+  res.send("Contact me: angels@gmail.com");
+});
+
+app.get("/about", function(req, res){
+  res.send("My name is Veronika, and I Love chips");
+});
+
+app.get("/hobbies", function(req, res){
+  res.send("<ul><li>Coffee</li><li>Code</li><li>Beer</li></ul>");
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
